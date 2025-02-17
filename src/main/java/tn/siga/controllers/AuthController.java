4c9955654883f2d7acfabd14644681e950b9d26d
azizzarr <75136140+azizzarr@users.noreply.github.com>
2025-02-17 13:39:13 +0100

    test

--- src/main/java/tn/siga/controllers/AuthController.java
+++ src/main/java/tn/siga/controllers/AuthController.java
@@ -102,7 +102,7 @@ public class AuthController {
         user.setRoles(roles);
         userRepository.save(user);
 
-        return ResponseEntity.ok(new MessageResponse("User registered successfully!!!"));
+        return ResponseEntity.ok(new MessageResponse("User registereed successfully!!!"));
     }
 
 
